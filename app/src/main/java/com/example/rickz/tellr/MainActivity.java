bc33b679fbe67010837ae102bc78fb09f4867893
wsxdrorange <rick.zhang@yahoo.com>
2017-06-29 03:01:45 -0500

    Weather Task Added

--- app/src/main/java/com/example/rickz/tellr/MainActivity.java
+++ app/src/main/java/com/example/rickz/tellr/MainActivity.java
@@ -14,20 +14,21 @@ import org.json.JSONObject;
 
 import java.net.URL;
 
-import static android.R.attr.name;
 
 public class MainActivity extends AppCompatActivity{
 
     WeatherTask weather;
+    String news;
+    String name = "Rick Zhang";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String news = "Hello " + name + ". Here is your daily update.";
+        news = "Hello " + name + ". Here is your daily update. ";
         weather = new WeatherTask();
         weather.execute("https://api.darksky.net/forecast/f87e64592db4c029937c3c7e92f9e115/33.2155920,-97.1460380");
-        Log.d("myTag","message");
+        Log.d("myTag","Finished On Create");
 
 
 
@@ -35,18 +36,31 @@ public class MainActivity extends AppCompatActivity{
         Button tellMe = (Button) findViewById(R.id.tellMe);
         tellMe.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                run();
+                addWeatherStatement();
+                System.out.println(news);
 
             }
         });
 
     }
-    public void run(){
-        JSONArray array = weather.getQuery();
+    public void addWeatherStatement(){
+        int minTemp,maxTemp,precip;
+        String summary;
+
+        JSONArray array = weather.getQuery(); //get JSON array from WeatherTask
         //System.out.println("ARRAY: \n" + array);
         try {
-            System.out.println(array.getJSONObject(0).getString("temperatureMin"));
-        } catch (JSONException e) {
+            //Add weather statement from JSCON object
+            minTemp = (int)Double.parseDouble(array.getJSONObject(0).getString("temperatureMin")); //get low Temp
+            maxTemp = (int)Double.parseDouble(array.getJSONObject(0).getString("temperatureMax")); //get High Temp
+            precip = (int)(Double.parseDouble(array.getJSONObject(0).getString("precipProbability"))*10.0); //get chance of rain
+            summary = array.getJSONObject(0).getString("summary").toString(); //get daily summary
+            //System.out.println(minTemp + "\n" + maxTemp + "\n" + precip + "\n" + summary);
+
+            //Update news message
+            news += "Today's weather will be " + summary + " The high for today is " + maxTemp + " degrees Fahrenheit, and the low for today is " + minTemp + ". There is a " + precip + " percent chance that it will rain today.";
+        }
+        catch (JSONException e) {
             e.printStackTrace();
         }
     }
