815b2443972098d33ea5690370cc0c71205bd4e3
wsxdrorange <rick.zhang@yahoo.com>
2017-06-30 01:20:53 -0500

    Location Service

--- app/src/main/java/com/example/rickz/tellr/LocationActivity.java
+++ app/src/main/java/com/example/rickz/tellr/LocationActivity.java
@@ -1,18 +1,15 @@
 package com.example.rickz.tellr;
 
 import android.content.IntentSender;
-import android.content.SyncStatusObserver;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.rickz.tellr.R;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
@@ -22,19 +19,10 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.net.URL;
-
-
-
-public class MainActivity extends AppCompatActivity implements ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
-
-    WeatherTask weather;
-    String news;
-    String name = "Rick Zhang";
+public class LocationActivity extends AppCompatActivity implements
+        ConnectionCallbacks,
+        OnConnectionFailedListener,
+        LocationListener {
 
     //Define a request code to send to Google Play services
     private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
@@ -43,12 +31,12 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
     private double currentLatitude;
     private double currentLongitude;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Location GPSTracker
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 // The next two lines tell the new client that “this” current class will handle connection stuff
                 .addConnectionCallbacks(this)
@@ -63,46 +51,7 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
                 .setInterval(10 * 1000)        // 10 seconds, in milliseconds
                 .setFastestInterval(1 * 1000); // 1 second, in milliseconds
 
-
-        news = "Hello " + name + ". Here is your daily update. ";
-        weather = new WeatherTask();
-        weather.execute("https://api.darksky.net/forecast/f87e64592db4c029937c3c7e92f9e115/" + currentLatitude + "," + currentLongitude);
-        Log.d("myTag", currentLatitude + " " + currentLatitude);
-
-
-        //Tell me Button
-        Button tellMe = (Button) findViewById(R.id.tellMe);
-        tellMe.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                addWeatherStatement();
-                System.out.println(news);
-
-            }
-        });
-
     }
-
-    public void addWeatherStatement() {
-        int minTemp, maxTemp, precip;
-        String summary;
-
-        JSONArray array = weather.getQuery(); //get JSON array from WeatherTask
-        //System.out.println("ARRAY: \n" + array);
-        try {
-            //Add weather statement from JSCON object
-            minTemp = (int) Double.parseDouble(array.getJSONObject(0).getString("temperatureMin")); //get low Temp
-            maxTemp = (int) Double.parseDouble(array.getJSONObject(0).getString("temperatureMax")); //get High Temp
-            precip = (int) (Double.parseDouble(array.getJSONObject(0).getString("precipProbability")) * 10.0); //get chance of rain
-            summary = array.getJSONObject(0).getString("summary").toString(); //get daily summary
-            //System.out.println(minTemp + "\n" + maxTemp + "\n" + precip + "\n" + summary);
-
-            //Update news message
-            news += "Today's weather will be " + summary + " The high for today is " + maxTemp + " degrees Fahrenheit, and the low for today is " + minTemp + ". There is a " + precip + " percent chance that it will rain today.";
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
 
     @Override
     protected void onResume() {
@@ -198,4 +147,5 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
         currentLatitude = location.getLatitude();
         currentLongitude = location.getLongitude();
     }
+
 }
