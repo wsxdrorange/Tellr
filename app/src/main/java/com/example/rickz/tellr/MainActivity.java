862fb1185847dee60764d8a04cca123d3e470864
wsxdrorange <rick.zhang@yahoo.com>
2017-07-06 00:17:20 -0500

    Debug

--- app/src/main/java/com/example/rickz/tellr/MainActivity.java
+++ app/src/main/java/com/example/rickz/tellr/MainActivity.java
@@ -44,24 +44,6 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         Log.d("DEBUG","START");
         news = "Hello " + name + ". Here is your daily update. ";
         weather = new WeatherTask();
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        Log.d("DEBUG","LocationManager Created");
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            Log.d("DEBUG","Permission Denied");
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            ActivityCompat.requestPermissions(this,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                    0);
-        }
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        currentLongitude = location.getLongitude();
-        currentLatitude = location.getLatitude();
         Log.d("DEBUG", currentLatitude + " " + currentLatitude);
         weather.execute("https://api.darksky.net/forecast/f87e64592db4c029937c3c7e92f9e115/" + currentLatitude + "," + currentLongitude);
 
